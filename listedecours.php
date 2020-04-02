a58e3110648236fec1f599ff29a922b416787b54
Essaid <eh1307@gmail.com>
2020-04-02 10:25:17 +0200

    page liste de cours.php

--- listedecours.php
+++ listedecours.php
@@ -3,7 +3,16 @@
 // Inclusion de la configuration de la base de données afin que ce fichier puisse faire les appels en base correctement
 include 'config/database.php';
 
+/*
+    Requête SQL permettant d'aller chercher tous les utilisateurs
+    dans la base de données
+*/
 
+$query = $pdo->query(
+    'SELECT  titre  FROM cours'
+);
+
+$listedecours = $query->fetchAll();
 
 // Affichage
 $PAGE = [
