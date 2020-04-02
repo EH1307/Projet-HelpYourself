877fe70e48c04fbd29eca9f149b4a9e2ac7bb294
Essaid <eh1307@gmail.com>
2020-04-02 13:13:49 +0200

    liste de cours.php

--- listedecours.php
+++ listedecours.php
@@ -4,7 +4,7 @@
 include 'config/database.php';
 
 /*
-    Requête SQL permettant d'aller chercher tous les utilisateurs
+    Requête SQL permettant d'aller chercher tous les cours
     dans la base de données
 */
 
