7d9d7db2f120b32de31fc94a9fb7052c503d8c1b
J.M. Cléry <jmclery@gmail.com>
2020-04-02 09:57:28 +0200

    Exemple d'utilisation de fetch()

--- index.php
+++ index.php
@@ -3,8 +3,16 @@
 // Inclusion de la configuration de la base de données afin que ce fichier puisse faire les appels en base correctement
 include 'config/database.php';
 
-$nom = "CLERY";
-$prenom = "JM";
+/*
+    Requête SQL qui va compter le nombre d'éléments
+    dans la table "utilisateurs" dans la base de données
+*/
+$query = $pdo->query(
+    'SELECT COUNT(*) AS nbUtilisateurs FROM utilisateurs'
+);
+$result = $query->fetch();
+
+$nbUtilisateurs = $result['nbUtilisateurs'];
 
 // Affichage
 $PAGE = [
