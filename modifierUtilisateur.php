610472ce74e5861e6ecc56ae071133a5201132d2
J.M. Cléry <jmclery@gmail.com>
2020-04-03 13:03:36 +0200

    Exemple pour page de modification

--- modifierUtilisateur.php
+++ modifierUtilisateur.php
@@ -3,7 +3,27 @@
 // Inclusion de la configuration de la base de données afin que ce fichier puisse faire les appels en base correctement
 include 'config/database.php';
 
+// Vérification de l'existence d'un ID dans l'URL
+if (!array_key_exists('id', $_GET)) {
+    header('Location:utilisateurs.php');
+    exit();
+}
 
+$id = $_GET['id'];
+
+// Récupération des infos de l'utilisateurs
+$query = $pdo->prepare(
+    'SELECT * FROM utilisateurs WHERE idUtilisateur = :id'
+);
+$query->bindParam(':id', $id, PDO::PARAM_INT);
+$query->execute();
+$utilisateur = $query->fetch();
+
+// Et Récupération de la liste des classes
+$query = $pdo->query(
+    'SELECT idClasse, nom FROM classes'
+);
+$classes = $query->fetchAll();
 
 // Affichage
 $PAGE = [
