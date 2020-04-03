cec94dbc6820a720f464a45c45ae0511849be2db
Abbou Walid <w.abbou@protonmail.com>
2020-04-03 11:37:28 +0200

    maj page ajoutUtilsateur.php

--- ajoutUtilisateur.php
+++ ajoutUtilisateur.php
@@ -9,12 +9,6 @@ $PAGE = [
     'template' => 'ajoutUtilisateur.phtml'
 ];
 
-$query = $pdo->query(
-    'SELECT idUtilisateur, nom, prenom FROM utilisateurs'
-);
-
-$utilisateurs = $query->fetchAll();
-
 /*
     Si le formulaire de cette page a été "submitted",
     on exécute ce "if"
@@ -22,37 +16,54 @@ $utilisateurs = $query->fetchAll();
 if ($_POST) {
     $name            = $_POST['name'];
     $firstname       = $_POST['firstname'];
+    $email           = $_POST['email'];
+    $enterpassword   = $_POST['enterpassword'];
+    $role            = $_POST['role'];
+    $idClasse        = $_POST['class'];
 
-// Insertion en base de données des informations
-$query = $pdo->prepare(
+
+    // Insertion en base de données des informations
+    $query = $pdo->prepare(
     "INSERT INTO
         utilisateurs (
+            idUtilisateur,
             nom,
-            prenom
+            prenom,
+            email,
+            motDePasse,
+            role,
+            idClasse
         ) VALUES (
+            NULL,
             :nom,
             :prenom
+            :email,
+            :motDePasse,
+            :role,
+            :idClasse
         )"
-); 
+    ); 
 
- // Les appels à 'bindParam' rendent le code plus verbeux, mais permettent de contrôler le type de la donnée qui entre dans chaque champs de la table
- $query->bindParam(':nom', $name, PDO::PARAM_STR);
- $query->bindParam(':prenom', $firstname, PDO::PARAM_STR);
+    // Les appels à 'bindParam' rendent le code plus verbeux, mais permettent de contrôler le type de la donnée qui entre dans chaque champs de la table
+    $query->bindParam(':nom', $name, PDO::PARAM_STR);
+    $query->bindParam(':prenom', $firstname, PDO::PARAM_STR);
+    $query->bindParam(':email', $email, PDO::PARAM_STR);
+    $query->bindParam(':motDePasse', $enterpassword, PDO::PARAM_STR);
+    $query->bindParam(':role', $role, PDO::PARAM_STR);
+    $query->bindParam(':idClasse', $idClasse, PDO::PARAM_INT);
 
+    $query->execute();
 
- $query->execute();
-
- echo "L'utilisateur a bien été créé !";
+    echo "L'utilisateur a bien été créé !";
 
 }
 
 
+$query = $pdo->query(
+    'SELECT idClasse, nom FROM classes'
+);
 
-/*
-    Requête SQL permettant d'aller chercher tous les utilisateurs
-    dans la base de données
-*/
-
+$classes = $query->fetchAll();
 
 // Affichage
 include 'integrations/MASTER.phtml';
