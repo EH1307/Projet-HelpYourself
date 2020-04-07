0745ed18ce593e72295d1b43a9249a44445298b2
rdad <akindofceasar@gmail.com>
2020-04-07 11:25:26 +0200

    Supression d'un utilisateur : preparation

--- supprimerUtilisateur.php
+++ supprimerUtilisateur.php
@@ -3,12 +3,18 @@
 // Inclusion de la configuration de la base de données afin que ce fichier puisse faire les appels en base correctement
 include 'config/database.php';
 
+// Suppression de l'utilisateur dans la base de donnée
+
+if (!array_key_exists('id', $_GET)) {
+  header('Location: listeDesUtilisateurs.php');
+  exit();
+}
+
+$id = $_GET['id'];
 
 
-// Affichage
-$PAGE = [
-    'title' => 'Supprimer un Utilisateur',
-    'template' => 'supprimerUtilisateur.phtml'
-];
 
-include 'integrations/MASTER.phtml';
+
+
+header('Location: listeDesUtilisateurs.php');
+exit();
