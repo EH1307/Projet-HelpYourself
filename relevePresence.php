f34d5e2662313b53282a4fbaf333f7db67b42fed
Essaid <eh1307@gmail.com>
2020-04-07 09:32:18 +0200

    maj listeDesUtilisateurs et relevePresence

--- relevePresence.php
+++ relevePresence.php
@@ -3,14 +3,27 @@
 // Inclusion de la configuration de la base de données afin que ce fichier puisse faire les appels en base correctement
 include 'config/database.php';
 
-// Requête SQL permettant d'aller chercher tous les utilisateurs
+// Requête SQL permettant d'aller chercher tous les cours , classes et utilisateurs
 //   dans la base de données
+
+$query = $pdo->prepare(
+    'SELECT * FROM cours'
+);
+$query->execute();
+$cours = $query->fetchAll();
+
+
+$query = $pdo->prepare(
+    'SELECT * FROM classes'
+);
+$query->execute();
+$classes = $query->fetchAll();
 
 
 $query = $pdo->query(
 'SELECT idUtilisateur, nom, prenom FROM utilisateurs'
 );
-
+$query->execute();
 $utilisateurs = $query->fetchAll();
 
 
