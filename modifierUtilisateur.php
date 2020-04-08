459576cc8e76b5ac08723624b6c7699ff77150e3
rdad <akindofceasar@gmail.com>
2020-04-08 09:32:50 +0200

    modifierUtilisateur : pret

--- modifierUtilisateur.php
+++ modifierUtilisateur.php
@@ -3,24 +3,26 @@
 // Inclusion de la configuration de la base de données afin que ce fichier puisse faire les appels en base correctement
 include 'config/database.php';
 
-print_r($_POST);
-exit();
 
 // Vérification de l'existence d'un ID dans l'URL
 
-if (!array_key_exists('id', $_GET)) {
-    header('Location:modifierUtilisateur.php');
+if (!array_key_exists('idUtilisateur', $_POST)) {
+    header('Location:listeDesUtilisateurs.php');
     exit();
 }
 
-$id = $_GET['id'];
+$idUtilisateur = $_POST['idUtilisateur'];
+$role = $_POST['role'];
+
 
 // Récupération des infos de l'utilisateurs
 $query = $pdo->prepare(
-    ''
+    'UPDATE'
 );
-$query->bindParam(':id', $id, PDO::PARAM_INT);
+$query->bindParam(':idUtilisateur', $idUtilisateur, PDO::PARAM_INT);
+$query->bindParam(':role', $role, PDO::PARAM_STR);
+
 $query->execute();
 
-header('Location: listeDesUtilisateurs.php');
-exit();
+//header('Location: listeDesUtilisateurs.php');
+//exit();
