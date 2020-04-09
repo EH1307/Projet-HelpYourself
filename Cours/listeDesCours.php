7f1b8b3bb0418884ea26d10850ff01446cba3e69
Essaid <eh1307@gmail.com>
2020-04-09 10:18:48 +0200

    liste des cours

--- Cours/listeDesCours.php
+++ Cours/listeDesCours.php
@@ -1,7 +1,7 @@
 <?php
 
 // Inclusion de la configuration de la base de données afin que ce fichier puisse faire les appels en base correctement
-include 'config/database.php';
+include '../config/database.php';
 
 /*
     Requête SQL permettant d'aller chercher tous les cours
@@ -29,4 +29,4 @@ $PAGE = [
 
 $navigation = "cour";
 
-include 'integrations/MASTER.phtml';
+include '../integrations/MASTER.phtml';
