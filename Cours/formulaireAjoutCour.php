20bf744cd3c446c1f63164a2bee7de0dcf34d8b0
Essaid <eh1307@gmail.com>
2020-04-10 12:27:05 +0200

    MAJ formulaire ajout cour

--- Cours/formulaireAjoutCour.php
+++ Cours/formulaireAjoutCour.php
@@ -9,47 +9,56 @@ $PAGE = [
     'template' => '../Cours/formulaireAjoutCour.phtml'
 ];
 
+$navigation = "cour";
+
+
+$query = $pdo->query(
+    'SELECT * FROM classes'
+);
+
+$classes = $query->fetchAll();
+
+
 /*
     Si le formulaire de cette page a été "submitted",
     on exécute ce "if"
 */
 if ($_POST) {
-    $title          = $_POST['titre'];
+    $titre          = $_POST['titre'];
     $dateDebut      = date("Y-m-d H:i:s",strtotime($_POST['dateDebut']));
     $dateFin        = date("Y-m-d H:i:s",strtotime($_POST['dateFin']));
-    $idClass        = $_POST['idClasse'];
-    $state          = $_POST['etat'];
+    $idClasse       = $_POST['idClasse'];
+    $etat          = $_POST['etat'];
     
-
+/*
     // ============================
     // Vérifications diverses liées à un ajout d'un cours
     $ERRORS = [];
     // ============================
 
     // 1. Est-ce que les champs sont vides ?
-    if (empty($title)) {
+    if (empty($titre)) {
         $ERRORS[] = 'Le champs "titre" doit être rempli';
     }
-    if (empty($beginDate)) {
+    if (empty($dateDebut)) {
         $ERRORS[] = 'Le champs "date de début" doit être rempli';
     }
-    if (empty($endDate)) {
+    if (empty($dateFin)) {
         $ERRORS[] = 'Le champs "date de fin" doit être rempli';
     }
-    if (empty($idClass)) {
+    if (empty($idClasse)) {
         $ERRORS[] = 'Le champs "classe" doit être rempli';
     }
-    if (empty($state)) {
+    if (empty($etat)) {
         $ERRORS[] = 'Le champs "état" doit être rempli';
     }
     
-
     // Si à ce stade on a détecté des erreurs, on s'arrête là et on renvoie les erreurs au client
     if (!empty($ERRORS)) {
         include('../integrations/MASTER.phtml');
         return; // Stoppe l'exécution du script ici !
     }
-
+*/
     // Insertion en base de données des informations
     $query = $pdo->prepare(
     "INSERT INTO
@@ -70,29 +79,23 @@ if ($_POST) {
     ); 
 
     // Les appels à 'bindParam' rendent le code plus verbeux, mais permettent de contrôler le type de la donnée qui entre dans chaque champs de la table
-    $query->bindParam(':titre', $title, PDO::PARAM_STR);
-    $query->bindParam(':dateDebut', $beginDate, PDO::PARAM_STR);
-    $query->bindParam(':dateFin', $endDate, PDO::PARAM_STR);
-    $query->bindParam(':idClasse', $idClass, PDO::PARAM_INT);
-    $query->bindParam(':etat', $state, PDO::PARAM_STR);
+    $query->bindParam(':titre', $titre, PDO::PARAM_STR);
+    $query->bindParam(':dateDebut', $dateDebut, PDO::PARAM_STR);
+    $query->bindParam(':dateFin', $dateFin, PDO::PARAM_STR);
+    $query->bindParam(':idClasse', $idClasse, PDO::PARAM_INT);
+    $query->bindParam(':etat', $etat, PDO::PARAM_STR);
     
 
     $query->execute();
 
     echo "Le cour a bien été créé !";
 
+    header('Location: listeDesCours.php');
+    exit();
+
 }
 
 
-$query = $pdo->query(
-    'SELECT idClasse, nom FROM classes'
-);
-
-$classes = $query->fetchAll();
-
-
 // Affichage
-
-$navigation = "cour";
 
 include '../integrations/MASTER.phtml';
