40b3e9acffbbb4e018ec039ff6a26a64d917330d
rdad <akindofceasar@gmail.com>
2020-04-14 10:43:48 +0200

    Connection : Emvoie du login et motdepasse

--- Accueil/connexion.php
+++ Accueil/connexion.php
@@ -3,6 +3,21 @@
 // Inclusion de la configuration de la base de données afin que ce fichier puisse faire les appels en base correctement
 include '../config/database.php';
 
+if ($_POST) {
+    $email        = $_POST['email'];
+    $motDePasse   = hash('sha256',$_POST['motDePasse']);
+
+    $query = $pdo->prepare(
+        'SELECT * FROM utilisateurs WHERE email=:email AND motDePasse=:motDePasse'
+    );
+    $query->bindParam(':email', $email, PDO::PARAM_STR);
+    $query->bindParam(':motDePasse', $motDePasse, PDO::PARAM_STR);
+    $query->execute();
+    $user = $query->fetch();
+
+    print_r($user);
+    exit();
+}
 
 
 // Affichage
